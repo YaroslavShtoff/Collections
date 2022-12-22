ab41217594425d7fdf8e6ef96229628d093d34f4
yarik <yarikshtoda1988@gmail.com>
2022-12-22 20:34:05 +0300

    ДЗ номер 3 из коллекции 1

--- Main.java
+++ Main.java
@@ -20,7 +20,7 @@ import java.util.List;
             }
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
-                    System.out.print(biDemArrList.get(i).get(j) + " ");
+                    System.out.print(biDemArrList.get(i).get(j) + "  ");
                 }
                 System.out.println();
             }
